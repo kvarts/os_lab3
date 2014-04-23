70ac1dc841d81a86b51056134aaf2c6088b55359
Kochergin Ivan <mr.kochergin.ivan@gmail.com>
2014-04-24 00:36:47 +0400

    Поправлен баг с буфером, изменена проверка на выход, + небольшие изменения

--- main.c
+++ main.c
@@ -13,11 +13,13 @@
 #define STR_SH_LINE "sh-> "
 
 int flag_wait = 1;
+char buffer[BUF_SIZE];
 
 void int_handler(int sig)
 {
 	write(1,"\n", 1);
 	write(1, STR_SH_LINE, sizeof(STR_SH_LINE));
+	memset(buffer, '/0', BUF_SIZE);
 	return;
 }
 
@@ -34,8 +36,6 @@ void chld_handler(int sig)
 		printf("killed by signal %d\n", WTERMSIG(status));
 	else if (WIFSTOPPED(status))
 		printf("stopped by signal %d\n", WSTOPSIG(status));
-	else if (WIFCONTINUED(status))
-		printf("continued\n");
 	write(1, STR_SH_LINE, sizeof(STR_SH_LINE));
 }
 
@@ -43,24 +43,22 @@ int main(int argc, char* argv[])
 {	
 	(void)signal(SIGINT, int_handler);
 	(void)signal(SIGCHLD, chld_handler);
-	char buffer[BUF_SIZE];
-	int len;
 	write(1, "Hello! Welcome to mini shell. Enter \"q\" to exit\n", 48);
 	while(1) {
-		memset(buffer, '\0', BUF_SIZE);
+		int len;
 		write(1, STR_SH_LINE, sizeof(STR_SH_LINE));
 		len = read(0, buffer, BUF_SIZE);
 		buffer[len-1] = '\0';
 		if(len == 1)
 			continue;
-		if (is_exit(buffer))
+		if (strlen(buffer) == 1 && buffer[0] == 'q')
 			_exit(EXIT_SUCCESS);
-		command(buffer);
+		command();
 	}
 	_exit(EXIT_SUCCESS);
 }
 
-int command(char *buffer, int len)
+int command(void)
 {
 	char *argv[MAX_ARGC];
 	char argc;
@@ -70,6 +68,8 @@ int command(char *buffer, int len)
 		buffer[strlen(buffer)-1] = '\0';
 		flag_wait = 0;
 	}
+	else
+		flag_wait = 1;
 	
 	/* start parse */
 	char i = 0;
@@ -88,8 +88,7 @@ int command(char *buffer, int len)
 	sigset_t sigmask;
 	if ((sigemptyset(&sigmask) == -1) || (sigaddset(&sigmask, SIGINT) == -1))
 			error_exit(errno);
-	if (sigprocmask(SIG_BLOCK, &sigmask, NULL) == -1)
-			error_exit(errno);
+	
 
 	pid_t pid;
 	pid = fork();
@@ -98,28 +97,24 @@ int command(char *buffer, int len)
 		case -1: 
 			error_exit(errno);
 		case 0:
-			if(flag_wait)
-				if (sigprocmask(SIG_UNBLOCK, &sigmask, NULL) == -1)
+			if(!flag_wait) {
+				if (sigprocmask(SIG_BLOCK, &sigmask, NULL) == -1)
 					error_exit(errno);
-			printf("\tProcces forked.\n\tNew PID = [%d]\n", getpid());
+				printf("\tNew proccess with PID = [%d]\n", getpid());
+			}
 			execvp(argv[0], argv);
-			error_exit(errno);
+			perror("Exec error!\n");
+			_exit(EXIT_FAILURE);
 		default:
 			if (flag_wait) {
 				int status;
 				(void) waitpid(pid, &status, 0);
 			}
+			memset(buffer, '/0', BUF_SIZE);
 			sleep(1);
 			break;
 	}
 	return EXIT_SUCCESS;
-}
-
-int is_exit(char *string)
-{
-	if (strlen(string) != 1)
-		return 0;
-	return !memcmp(string, "q", 1);
 }
 
 int error_exit(int id_error)
